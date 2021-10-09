20d0b10a674ee5ec9d03215e840a2eec6fa62d72
Vilo4ka <saiviolet@gmail.com>
2021-10-09 16:04:19 +0300

    vers 768px не полная, без слайдера и футера

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,36 @@
-const swiper = new Swiper(".team__swiper", {
-  effect: "flip",
-  grabCursor: true,
-  pagination: {
-    el: ".swiper__pagination",
-    clickable: true,
-    bulletClass: "swiper__bullet",
-    bulletActiveClass: "swiper__bullet-active",
-  },
+
+window.addEventListener('resize', function(evt){
+  const width = document.body.clientWidth;
+  console.log(width);
+  if (width <= 320) {
+    const swiper = new Swiper(".team__swiper", {
+      grabCursor: true,
+    //для 320
+      effect: "flip",
+      pagination: {
+        el: ".swiper__pagination",
+        clickable: true,
+        bulletClass: "swiper__bullet",
+        bulletActiveClass: "swiper__bullet-active",
+      },
+    });
+  }
+  if (width > 320) {
+    const swiper = new Swiper(".team__swiper", {
+      grabCursor: true,
+      slidesPerView: 1.5,
+      spaceBetween: 28,
+      pagination: {
+        el: ".swiper__pagination",
+        clickable: true,
+        },
+        watchOverflow: true,
+    });
+  }
 });
+
+
+
 
 const swiper_battles = new Swiper(".battles__swiper", {
   effect: "flip",
