fb3285a10b7189c5c603b5512a639c6cff0db167
Vilo4ka <saiviolet@gmail.com>
2021-10-10 20:14:16 +0300

    vers 768px не полная

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,30 @@
-
-window.addEventListener('resize', function(evt){
-  const width = document.body.clientWidth;
-  console.log(width);
-  if (width <= 320) {
-    const swiper = new Swiper(".team__swiper", {
-      grabCursor: true,
-    //для 320
-      effect: "flip",
-      pagination: {
-        el: ".swiper__pagination",
-        clickable: true,
-        bulletClass: "swiper__bullet",
-        bulletActiveClass: "swiper__bullet-active",
-      },
-    });
-  }
-  if (width > 320) {
-    const swiper = new Swiper(".team__swiper", {
-      grabCursor: true,
-      slidesPerView: 1.5,
+const width = document.body.clientWidth;
+const swiper = new Swiper(".team__swiper", {
+  grabCursor: true,
+  pagination: {
+    el: ".swiper__pagination",
+    clickable: true,
+    bulletClass: "swiper__bullet",
+    bulletActiveClass: "swiper__bullet-active",
+    
+  },
+  breakpoints: {
+    320: {slidesPerView: 1},
+    340: {
+      slidesPerView: 1.2,
+      spaceBetween: 18,
+      watchOverflow: true,
+    },
+    // when window width is >= 640px
+    768: {
+      slidesPerView: 2.1,
       spaceBetween: 28,
-      pagination: {
-        el: ".swiper__pagination",
-        clickable: true,
-        },
-        watchOverflow: true,
-    });
+      watchOverflow: true,
+    }
   }
 });
 
-
-
-
 const swiper_battles = new Swiper(".battles__swiper", {
-  effect: "flip",
   grabCursor: true,
   pagination: {
     el: ".swiper__pagination",
