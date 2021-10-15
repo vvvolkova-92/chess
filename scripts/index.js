648f7ee240a1b6425a50862dd256d173193f1c92
Vilo4ka <saiviolet@gmail.com>
2021-10-15 23:30:50 +0300

    js для окна

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,10 @@
-const swiper = new Swiper(".team__swiper", {
+const formRegistration = document.querySelector('.form'),
+buttonRegistrationFooter = document.querySelector('.button_position_footer'),
+buttonRegistrationHeader = document.querySelector('.button_position_header'),
+buttonRegistrationForm = document.querySelector('.button_position_reg-form'),
+buttonRegistrationFormCancel = document.querySelector('.button_type_cancel'),
+popupRegistration = document.querySelector('.registration-window'),
+swiper = new Swiper(".team__swiper", {
   grabCursor: true,
   loop: true,
   pagination: {
@@ -66,4 +72,25 @@ const battlesSwiper = new Swiper(".battles__swiper", {
       watchOverflow: true,
     }
   }
+});
+
+
+function openPopup(popup) {
+  popup.classList.add('registration-window_opened');
+};
+
+function closePopup(popup) {
+  popup.classList.remove('registration-window_opened');
+};
+
+buttonRegistrationHeader.addEventListener('click', () => {
+  openPopup(popupRegistration); 
+});
+
+buttonRegistrationFooter.addEventListener('click', () => {
+  openPopup(popupRegistration); 
+});
+
+buttonRegistrationFormCancel.addEventListener('click', () => {
+  closePopup(popupRegistration); 
 });
